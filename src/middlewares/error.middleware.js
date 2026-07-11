e766d09ea5ffd9cb55a27b3ed0bee4bf8ffcec0f
vaibhavjangam001 <vaibhavjangam001@gmail.com>
2026-07-11 22:00:33 +0530

    fix : errors response

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -1,11 +1,13 @@
 const errorHandler = (err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal server error";
+  const errors = err.errors || [];
 
   return res.status(statusCode).json({
     statusCode,
     success: false,
     message,
+    errors,
   });
 };
 
